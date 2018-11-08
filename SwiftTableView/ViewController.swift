d9b7a1652b725ccb843a3ed9c9c272e0433b8f6c
chenglong <chenglong@princeegg.com>
2018-11-08 16:48:24 +0800

    闭包 逃逸闭包 使用

--- SwiftTableView/ViewController.swift
+++ SwiftTableView/ViewController.swift
@@ -14,11 +14,41 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        text2.size(bb: { (b) in
+            
+        }) { (c) in
+            
+        }
+        let add : (_ a :Int,_ b :Int)->Int = {
+            (a,b) in
+            return a+b
+        }
+       let r = add(2,3)
+        print(r)
+        
+        textCloture(test1: { (test1) in
+            print(test1)
+        }, test2: {(test2) in
+            print(test2)
+        })
+        
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView .setEditing(true, animated: true)
         
     }
+    
+    func textCloture(test1 test1:(String)->(),test2 test2:@escaping(String)->()) -> Void {
+        test1("test1")
+        DispatchQueue.global().async {
+            DispatchQueue.main.async {
+//                test1("test11")
+                test2("test2")
+            }
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
     }
--- SwiftTableView/text2.swift
+++ SwiftTableView/text2.swift
@@ -8,10 +8,36 @@
 
 import UIKit
 
-class text2: UIView {
-    func size() {
-        
+struct  Person{
+    enum Address {
+        case success(String)
+        case failture(Int)
     }
+    func changeAddress(address : Address) {
+        switch address {
+        case .success(let value):
+            print(value)
+            break
+        case .failture(let age):
+            print(age)
+            break
+        default:
+            break
+        }
+    }
+}
+
+class text2: UIView {
+    
+    class func size(bb b:(String)->(),aa a : @escaping (String)->()) ->(){
+        var block = ((String)->()).self
+        b("ddd")
+        a("aaa")
+        
+//        return a(b)
+    }
+    
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
